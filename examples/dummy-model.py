30177acccccecf17545acdfd7f718cd4a784c739
Mario Alberich <malberich@gmail.com>
2016-07-02 20:34:09 +0200

    Added dummy model for fit/predict testing

---
+++ examples/dummy-model.py
@@ -0,0 +1,15 @@
+from minteressa.etl.connectors.KafkaConnector import KafkaModelConnector
+from minteressa.model.DummyModel import DummyModel
+
+if __name__ == '__main__':
+    topic_id = "ad32e3c1"
+    kfk = KafkaModelConnector(
+        group_id="topic-%s" % topic_id,
+        unlabeled_consumer_topic="unique_tweets-%s" % topic_id,
+        labeled_consumer_topic="labeled-%s" % topic_id,
+        label_request_producer_topic="label_request-%s" % topic_id
+    )
+
+    lang_filter = DummyModel(
+        connector=kfk
+    )
