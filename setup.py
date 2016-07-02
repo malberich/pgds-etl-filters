d52e5f9e5427e82bf415faeb87584ae37f5dd5bc
Mario Alberich <malberich@gmail.com>
2016-07-02 12:05:30 +0200

    Base setup script to install the module

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+    name="minteressa",
+    version='0.1',
+    packages=[
+        "minteressa",
+        "minteressa.etl",
+        "minteressa.etl.connectors"
+    ]
+)
