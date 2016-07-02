92048be0c6770de20b9239e09e175b784c312c4a
Mario Alberich <malberich@gmail.com>
2016-07-02 20:47:25 +0200

    unlabeled tweets come from raw/unique tweets, all of them at once

--- examples/dummy-model.py
+++ examples/dummy-model.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     topic_id = "ad32e3c1"
     kfk = KafkaModelConnector(
         group_id="topic-%s" % topic_id,
-        unlabeled_consumer_topic="unique_tweets-%s" % topic_id,
+        unlabeled_consumer_topic="unique_tweets",
         labeled_consumer_topic="labeled-%s" % topic_id,
         label_request_producer_topic="label_request-%s" % topic_id
     )
